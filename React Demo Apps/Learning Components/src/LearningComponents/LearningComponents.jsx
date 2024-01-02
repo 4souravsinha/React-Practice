931af7159feb75e9d6697516be01696204da2a69
Sourav Sinha <4souravsinhain@gmail.com>
2024-01-02 16:42:17 +0530

    add router for better navigation renamed App component to LearningComponents because that's what we did. Will create a new / component for welcome

--- React Demo Apps/Learning Components/src/LearningComponents/LearningComponents.jsx
+++ React Demo Apps/Learning Components/src/LearningComponents/LearningComponents.jsx
@@ -1,18 +1,17 @@
-import './App.css'
 import DogTile from './DogTile'
 
 
-function App() {
+function LearningComponents() {
   return (
-    <>
+    <div style={{display:"flex", gap:'5rem'}}>
       <DogTile imgURL="https://wtamu.edu/~cbaird/sq/images/dog2.jpg" imgDescription={"Hello From Doge"}>
         <p>Miwek</p>
       </DogTile>
       <DogTile imgURL="https://wtamu.edu/~cbaird/sq/images/dog2.jpg" imgDescription="A smiling Norwich Terrier">
         {/* <p>Dogeshwari</p> */}
       </DogTile>
-    </>
+    </div>
   )
 }
 
-export default App
+export default LearningComponents
--- React Demo Apps/Learning Components/src/main.jsx
+++ React Demo Apps/Learning Components/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import LearningComponents from './LearningComponents/LearningComponents.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LearningComponents />
+  },
+  {
+    path: '/learning-components',
+    element: <LearningComponents />
+  },
+
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
